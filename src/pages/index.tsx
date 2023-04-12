f24a7296c78946de1e36eccbbb5207c95bddb619
yakuhito <y@kuhi.to>
2023-04-12 14:46:18 -0700

    lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
     <Layout>
       <h1 className="text-3xl font-bold mb-4">Testing... 1... 2... 3...</h1>
       <p className="mb-6 text-left">
-        Uh oh! You've accessed this site too early and caught us unprepared. We're still working on the UI, so please come back later :)
+        Uh oh! You&apos;ve accessed this site too early and caught us unprepared. We&apos;re still working on the UI, so please come back later :)
       </p>
       <Link href="/faq">
         <p className="text-gray-600 underline">FAQ &rarr;</p>
