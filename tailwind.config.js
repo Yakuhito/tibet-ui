0fd27ef50c82b92cfc6d0a75c2b92319137f73cd
Jonathan <jonathanlangton3@gmail.com>
2023-05-02 17:53:22 +0100

    Added brand colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,21 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brandDark: '#526e78',
+        brandLight: '#EFF4F7',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        }
+      },
+      animation: {
+        fadeIn: 'fadeIn .3s ease-in-out',
+      },
+    },
   },
   plugins: [],
 }
