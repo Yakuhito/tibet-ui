ea4880b7331bb9c9c3d28d0965c29bb34917f2c1
Jonathan <jonathanlangton3@gmail.com>
2023-05-02 18:34:11 +0100

    Converted to new UI

--- src/components/BooleanSwitch.tsx
+++ src/components/BooleanSwitch.tsx
@@ -11,7 +11,7 @@ type BooleanSwitchProps = {
 const BooleanSwitch: React.FC<BooleanSwitchProps> = ({ disabled, onChange, isSelected, trueLabel, falseLabel }) => {
   return (
     <div className="">
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center bg-brandDark/10 p-1 rounded-xl font-medium mb-4">
           <button
             onClick={() => onChange(true)}
             disabled={disabled}
@@ -19,9 +19,9 @@ const BooleanSwitch: React.FC<BooleanSwitchProps> = ({ disabled, onChange, isSel
               disabled ?
                 'bg-gray-200 cursor-not-allowed' :
               isSelected
-                ? 'bg-green-500 border-green-500 text-white'
-                : 'bg-white border-gray-300 text-black'
-            } px-4 py-2 rounded-l-md focus:outline-none w-full border`}
+                ? 'bg-green-800 text-white'
+                : 'text-brandDark'
+            } px-4 py-2 rounded-lg focus:outline-none w-full`}
           >
             {trueLabel}
           </button>
@@ -32,9 +32,9 @@ const BooleanSwitch: React.FC<BooleanSwitchProps> = ({ disabled, onChange, isSel
               disabled ?
                 'bg-gray-200 cursor-not-allowed' :
               isSelected
-                ? 'bg-white border-gray-300 text-black'
-                : 'bg-red-500 border-red-500 text-white'
-            } px-4 py-2 rounded-r-md focus:outline-none w-full border`}
+                ? 'text-brandDark'
+                : 'bg-red-700 text-white'
+            } px-4 py-2 rounded-lg focus:outline-none w-full`}
           >
             {falseLabel}
           </button>
