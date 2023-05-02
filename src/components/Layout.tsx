4234d8e69feb6b8ed4421c752101a9c82aaec8dc
Jonathan <jonathanlangton3@gmail.com>
2023-05-02 18:05:39 +0100

    Removed logo component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,3 @@
-import Logo from './Logo';
-
 interface LayoutProps {
   children: React.ReactNode;
   isHomePage?: boolean;
@@ -8,9 +6,6 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children, isHomePage = false }) => {
   return (
     <div className={`flex flex-col min-h-screen mx-auto max-w-screen-md ${isHomePage ? 'justify-center' : ''}`}>
-      <header className="py-10 text-center">
-        <Logo />
-      </header>
       <main className={`flex flex-col items-center ${isHomePage ? 'justify-center' : ''} flex-grow`}>{children}</main>
       <footer className="py-6 text-center text-gray-600">
         <p>
