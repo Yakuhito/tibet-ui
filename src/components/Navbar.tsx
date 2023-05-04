974d139c78caf7489620297c4c0d42525c5a6fe9
Jonathan <jonathanlangton3@gmail.com>
2023-05-04 23:24:08 +0100

    Swap link will display as active page when on dynamic route e.g. /USDS

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,14 @@ import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { useState, useEffect } from 'react';
+
 
 export default function Navbar() {
   const router = useRouter();
+  useEffect(() => {
+    console.log(router.asPath, '🫧', router.asPath === "/[pair_short_name]")
+  }, [router])
   return (
     <nav className="sticky w-full top-0 bg-brandLight/50 dark:bg-zinc-900/50 backdrop-blur-xl">
       <div className="container mx-auto px-4 flex gap-8 items-center justify-between py-4">
@@ -18,7 +23,7 @@ export default function Navbar() {
             />
         </Link>
         <div className="flex items-center bg-brandDark/10 dark:bg-brandDark/20 rounded-xl p-1">
-          <Link href="/" className={`font-medium text-brandDark px-4 py-1 rounded-lg ${router.asPath === "/" ? 'text-brandLight bg-brandDark' : 'dark:text-brandLight/50'}`}>Swap</Link>
+          <Link href="/" className={`font-medium text-brandDark px-4 py-1 rounded-lg ${router.asPath === "/" || router.pathname === "/[pair_short_name]" ? 'text-brandLight bg-brandDark' : 'dark:text-brandLight/50'}`}>Swap</Link>
           <Link href="/faq" className={`font-medium text-brandDark px-4 py-1 rounded-lg ${router.asPath === "/faq" ? 'text-brandLight bg-brandDark' : 'dark:text-brandLight/50'}`}>FAQ</Link>
           <Link href={`${process.env.NEXT_PUBLIC_INFO_BASE_URL}`} className="font-medium text-brandDark dark:text-brandLight/50 px-4 py-1">Analytics</Link>
         </div>
