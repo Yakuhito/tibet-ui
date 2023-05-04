41a5a9ff8304cb17aa94de16b9ef0d90c42851f1
Jonathan <jonathanlangton3@gmail.com>
2023-05-04 23:11:21 +0100

    Moved selectedToken state variable and getTokens function up here for new pre-select feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,27 @@
+import { Token, getAllTokens } from '../api';
 import TabContainer from '../components/TabContainer';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Home: React.FC = () => {
+  const [selectedToken, setSelectedToken] = useState<Token | null>(null);
   const [pairLauncherId, setPairLauncherId] = useState<string | null>(null);
   const link = process.env.NEXT_PUBLIC_INFO_BASE_URL + (
     pairLauncherId === null ? "" : `/pair/${pairLauncherId}`
   );
 
+  // Fetch all tokens
+  const [tokens, setTokens] = useState<Token[] | null>(null);
+  useEffect(() => {
+    async function fetchTokens() {
+      const allTokens = await getAllTokens();
+      setTokens(allTokens);
+    }
+    fetchTokens();
+  }, []);
+
   return (
       <main className="max-w-[28rem]">
-        <TabContainer onPairSelect={setPairLauncherId} />
+        <TabContainer tokens={tokens} onPairSelect={setPairLauncherId} selectedToken={selectedToken} setSelectedToken={setSelectedToken} />
       </main>
   );
 };
