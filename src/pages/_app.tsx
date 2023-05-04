f53e0200fb57ab75aa859431d11e37fd3a01d2d2
Jonathan <jonathanlangton3@gmail.com>
2023-05-05 00:56:35 +0100

    Modified footer content

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,14 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </div>
       <footer className="pb-6 pt-1 text-center text-brandDark mt-auto">
-        <p>
-          Built with 💚 by the
-          <a href="https://twitter.com/yakuh1t0" target="_blank" rel="noopener noreferrer"> yak </a>
-          & GPT-4 | Powered by
-          <a href="https://fireacademy.io" target="_blank" rel="noopener noreferrer" className="underline mx-1">FireAcademy.io</a>
-          | Follow us on
-          <a href="https://twitter.com/TibetSwap" target="_blank" rel="noopener noreferrer" className="underline ml-1">Twitter</a>
-        </p>
+          <p>Follow us on<a href="https://twitter.com/TibetSwap" target="_blank" rel="noopener noreferrer" className="underline ml-1">Twitter</a></p>
       </footer>
     </div>
   );
