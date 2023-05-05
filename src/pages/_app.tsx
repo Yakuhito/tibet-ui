96ef0f64f8a3576e8fdbb32faf965f5be61ca89c
Jonathan <jonathanlangton3@gmail.com>
2023-05-06 00:40:52 +0100

    Added padding to top of footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <div className="pt-12">
           <Component {...pageProps} />
         </div>
-        <footer className="pb-6 pt-1 text-center text-brandDark mt-auto">
+        <footer className="pb-6 pt-12 text-center text-brandDark mt-auto">
               <p>Follow us on<a href="https://twitter.com/TibetSwap" target="_blank" rel="noopener noreferrer" className="underline ml-1">Twitter</a></p>
         </footer>
       </div>
