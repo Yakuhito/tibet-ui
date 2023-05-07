a2a353aad067094d7c22a75678565b73de49c743
Jonathan <jonathanlangton3@gmail.com>
2023-05-08 00:07:19 +0100

    Added space to stop dynamic class names forming together, invalidating them

--- src/components/GenerateOfferButton.tsx
+++ src/components/GenerateOfferButton.tsx
@@ -8,7 +8,7 @@ const GenerateOfferButton: React.FC<GenerateOfferButtonProps> = ({ disabled, onP
   return (
     <button
       onClick={onPressed}
-      className={`${disabled ? 'bg-brandDark/10 text-brandDark/20 dark:text-brandLight/30 cursor-not-allowed' : (isBuySelected ? 'bg-green-800' : 'bg-red-700')}text-brandLight px-4 py-2 rounded-lg w-full mt-8 font-medium`}
+      className={`${disabled ? 'bg-brandDark/10 text-brandDark/20 dark:text-brandLight/30 cursor-not-allowed' : (isBuySelected ? 'bg-green-800' : 'bg-red-700')} text-brandLight px-4 py-2 rounded-lg w-full mt-8 font-medium`}
       disabled={disabled}
     >
       Generate Offer
