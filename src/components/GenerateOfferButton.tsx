4120e343e4de1b590e59e3ce7902d4cf4c865523
Jonathan <jonathanlangton3@gmail.com>
2023-05-07 23:47:29 +0100

    Small cleanup

--- src/components/GenerateOfferButton.tsx
+++ src/components/GenerateOfferButton.tsx
@@ -1,22 +1,14 @@
-import React from 'react';
-
 type GenerateOfferButtonProps = {
   disabled: boolean;
   isBuySelected: boolean;
   onPressed: () => void;
 };
 
-const GenerateOfferButton: React.FC<GenerateOfferButtonProps> = ({
-  disabled,
-  onPressed,
-  isBuySelected,
-}) => {
+const GenerateOfferButton: React.FC<GenerateOfferButtonProps> = ({ disabled, onPressed, isBuySelected,}) => {
   return (
     <button
       onClick={onPressed}
-      className={`${
-        disabled ? 'bg-brandDark/10 text-brandDark/20 dark:text-brandLight/30 cursor-not-allowed' : (isBuySelected ? 'bg-green-800' : 'bg-red-700')
-      } text-brandLight px-4 py-2 rounded-lg w-full mt-8 font-medium`}
+      className={`${disabled ? 'bg-brandDark/10 text-brandDark/20 dark:text-brandLight/30 cursor-not-allowed' : (isBuySelected ? 'bg-green-800' : 'bg-red-700')}text-brandLight px-4 py-2 rounded-lg w-full mt-8 font-medium`}
       disabled={disabled}
     >
       Generate Offer
