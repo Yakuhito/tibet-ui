69fdd1b991ea7ebd8b27a803ff7159bc74800a1d
Jonathan <jonathanlangton3@gmail.com>
2023-05-12 18:50:28 +0100

    Clean-up

--- src/components/SuccessScreen.tsx
+++ src/components/SuccessScreen.tsx
@@ -10,20 +10,22 @@ interface SuccessScreenProps {
 }
 
 function SuccessScreen({ offerData, devFee }: SuccessScreenProps) {
-    const [isShowing, setIsShowing] = useState(true)
+    const [isShowing, setIsShowing] = useState(true) // true when logo animation is happening
 
+    // Display confetti after logo animation
     useEffect(() => {
       const handleConfetti = () => {
         confetti({
-          particleCount: 100,  // Number of confetti particles
-          spread: 70,         // Spread of the confetti
-          origin: { y: 1 }, // Start position of the confetti (from the top)
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 1 }, // Start position of the confetti (from the top 0-1)
         });
       };
 
       !isShowing ? handleConfetti() : '';
     }, [isShowing]);
       
+    // Hide logo animation & show transaction result by setting isShowing state
     useEffect(() => {
         const timer = setTimeout(() => {
             setIsShowing(false);
@@ -36,6 +38,8 @@ function SuccessScreen({ offerData, devFee }: SuccessScreenProps) {
 
     return ( 
         <div className="flex justify-center items-center relative">
+
+            {/* Logo animation */}
             <Transition
                 as={Fragment}
                 show={isShowing}
@@ -51,7 +55,7 @@ function SuccessScreen({ offerData, devFee }: SuccessScreenProps) {
             </Transition>
 
 
-
+            {/* Show transaction details */}
             <Transition
                 as={Fragment}
                 show={!isShowing}
@@ -63,26 +67,27 @@ function SuccessScreen({ offerData, devFee }: SuccessScreenProps) {
                 leaveFrom="opacity-100 rotate-0 scale-100 "
                 leaveTo="opacity-0 scale-95 "
             >
-            <div className={`absolute overflow-x-clip top-0 w-full p-8 py-12 flex flex-col justify-center bg-brandDark bg-gradient-to-br from-[#7fa9b8] to-brandDark dark:from-brandDark dark:to-[#152f38] rounded-xl`}>
-                <p className="text-brandLight text-5xl mb-4 font-bold">Sent</p>
-                {offerData.offer.map(asset => (
-                    <div key={asset[0].asset_id} className="text-brandLight flex items-center gap-2 font-medium">
-                        <Image src={asset[0].image_url} width={30} height={30} alt="Token logo" className="rounded-full" />
-                        <p>{offerData.action === "SWAP" ? (asset[2] / Math.pow(10, asset[1] ? 12 : 3)) * (1+devFee) : (asset[2] / Math.pow(10, asset[1] ? 12 : 3))}</p>
-                        <p>{asset[0].short_name}</p>
-                    </div>
-                ))}
+                <div className={`absolute overflow-x-clip top-0 w-full p-8 py-12 flex flex-col justify-center bg-brandDark bg-gradient-to-br from-[#7fa9b8] to-brandDark dark:from-brandDark dark:to-[#152f38] rounded-xl`}>
+                    <p className="text-brandLight text-5xl mb-4 font-bold">Sent</p>
+                    {offerData.offer.map(asset => (
+                        <div key={asset[0].asset_id} className="text-brandLight flex items-center gap-2 font-medium">
+                            <Image src={asset[0].image_url} width={30} height={30} alt="Token logo" className="rounded-full" />
+                            <p>{offerData.action === "SWAP" ? (asset[2] / Math.pow(10, asset[1] ? 12 : 3)) * (1+devFee) : (asset[2] / Math.pow(10, asset[1] ? 12 : 3))}</p> {/* If transaction type is swap, add dev fee on top of offer amount */}
+                            <p>{asset[0].short_name}</p>
+                        </div>
+                    ))}
 
-                <p className="text-brandLight text-5xl mb-4 mt-12 font-bold">Received</p>
-                {offerData.request.map(asset => (
-                    <div key={asset[0].asset_id} className="text-brandLight flex items-center gap-2 font-medium pb-2">
-                        <Image src={asset[0].image_url} width={30} height={30} alt="Token logo" className="rounded-full" />
-                        <p>{(asset[2] / Math.pow(10, asset[1] ? 12 : 3))}</p>
-                        <p>{asset[0].short_name}</p>
-                    </div>
-                ))}
-            </div>
+                    <p className="text-brandLight text-5xl mb-4 mt-12 font-bold">Received</p>
+                    {offerData.request.map(asset => (
+                        <div key={asset[0].asset_id} className="text-brandLight flex items-center gap-2 font-medium pb-2">
+                            <Image src={asset[0].image_url} width={30} height={30} alt="Token logo" className="rounded-full" />
+                            <p>{(asset[2] / Math.pow(10, asset[1] ? 12 : 3))}</p>
+                            <p>{asset[0].short_name}</p>
+                        </div>
+                    ))}
+                </div>
             </Transition>
+            
         </div>
      );
 }
