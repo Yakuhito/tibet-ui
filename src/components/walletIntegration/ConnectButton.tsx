6eedb350882e42783f1e46b2244d6aaa5fbcf51a
Jonathan <jonathanlangton3@gmail.com>
2023-05-21 17:37:39 +0100

    Added currently connected wallet icon

--- src/components/walletIntegration/ConnectButton.tsx
+++ src/components/walletIntegration/ConnectButton.tsx
@@ -1,10 +1,7 @@
 import ConnectWalletModal from './ConnectedWalletModal';
-import { useState, useEffect, useContext } from 'react';
-import toast from 'react-hot-toast';
-import * as React from 'react';
-import WalletManager from '@/utils/walletIntegration/walletManager';
-import WalletIntegrationInterface from '@/utils/walletIntegration/walletIntegrationInterface';
 import WalletContext from '@/context/WalletContext';
+import { useState, useContext } from 'react';
+import Image from 'next/image';
 
 
 function ConnectButton() {
@@ -14,7 +11,10 @@ function ConnectButton() {
 
     return ( 
         <>
-            <button onClick={() => setIsWalletModalOpen(true)} className="bg-brandDark/10 dark:bg-brandDark/20 text-brandDark px-4 py-2 font-medium rounded-xl animate-fadeIn hover:opacity-80 dark:text-brandLight/50">{!activeWallet ? 'Connect Wallet' : 'Manage Wallet'}</button>
+            <button onClick={() => setIsWalletModalOpen(true)} className="flex items-center gap-2 bg-brandDark/10 dark:bg-brandDark/20 text-brandDark px-4 py-2 font-medium rounded-xl animate-fadeIn hover:opacity-80 dark:text-brandLight/50">
+                {activeWallet?.image && <Image src={activeWallet.image} width={20} height={20} alt={`${activeWallet?.name} wallet logo`} className="rounded-full" />}
+                {!activeWallet ? 'Connect Wallet' : 'Manage Wallet'}
+            </button>
             <ConnectWalletModal isOpen={isWalletModalOpen} setIsOpen={setIsWalletModalOpen} walletManager={walletManager} activeWallet={activeWallet} />
         </>
      );
