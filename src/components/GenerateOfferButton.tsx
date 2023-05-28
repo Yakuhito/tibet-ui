3c027bc93cda518a91b25915503a6f05d480a61d
Jonathan <jonathanlangton3@gmail.com>
2023-05-28 01:52:46 +0100

    Minor UI improvements

--- src/components/GenerateOfferButton.tsx
+++ src/components/GenerateOfferButton.tsx
@@ -8,7 +8,7 @@ const GenerateOfferButton: React.FC<GenerateOfferButtonProps> = ({ disabled, onP
   return (
     <button
       onClick={onPressed}
-      className={`${disabled ? 'bg-brandDark/10 text-brandDark/20 dark:text-brandLight/30 cursor-not-allowed' : (isBuySelected ? 'bg-green-800' : 'bg-red-700')} text-brandLight p-4 rounded-xl w-full mt-8 font-medium`}
+      className={`${disabled ? 'bg-brandDark/10 text-brandDark/20 dark:text-brandLight/30 cursor-not-allowed' : (isBuySelected ? 'bg-green-800 hover:opacity-95' : 'bg-red-700 hover:opacity-95')} text-brandLight p-4 rounded-xl w-full mt-8 font-medium`}
       disabled={disabled}
     >
       Swap
