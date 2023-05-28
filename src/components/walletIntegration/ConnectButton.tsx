71a9af9ac645d3adaa8e7e592680063af225d581
Jonathan <jonathanlangton3@gmail.com>
2023-05-28 07:36:47 +0100

    Styling improvements

--- src/components/walletIntegration/ConnectButton.tsx
+++ src/components/walletIntegration/ConnectButton.tsx
@@ -9,11 +9,22 @@ function ConnectButton() {
     const [isWalletModalOpen, setIsWalletModalOpen] = useState(false)
     const { walletManager, activeWallet } = useContext(WalletContext);
 
+    const displayAddress = () => {
+        if (activeWallet) {
+            const address = activeWallet.getAddress()
+            if (address && process.env.NEXT_PUBLIC_XCH) {
+                const short_address = address.slice(0, 7) + '...' + address.slice(-4)
+                return short_address ? short_address : 'Manage Wallet';
+            }
+        }
+        return 'Manage Wallet';
+    }
+
     return ( 
         <>
-            <button onClick={() => setIsWalletModalOpen(true)} className="flex items-center gap-2 bg-brandDark/10 dark:bg-brandDark/20 text-brandDark px-4 py-2 font-medium rounded-xl animate-fadeIn hover:opacity-80 dark:text-brandLight/50">
+            <button onClick={() => setIsWalletModalOpen(true)} className="flex items-center gap-2 bg-brandDark/10  text-brandDark dark:text-brandLight px-6 py-1.5 font-medium rounded-xl animate-fadeIn hover:opacity-80">
                 {activeWallet?.image && <Image src={activeWallet.image} width={20} height={20} alt={`${activeWallet?.name} wallet logo`} className="rounded-full" />}
-                {!activeWallet ? 'Connect Wallet' : 'Manage Wallet'}
+                {!activeWallet ? 'Connect Wallet' : displayAddress()}
             </button>
             <ConnectWalletModal isOpen={isWalletModalOpen} setIsOpen={setIsWalletModalOpen} walletManager={walletManager} activeWallet={activeWallet} />
         </>
