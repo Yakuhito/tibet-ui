0e8a2b6bf17632f1bb3ce107f1771cff6fb7fe6e
Jonathan <jonathanlangton3@gmail.com>
2023-05-30 16:15:49 +0100

    Pass fee param to Hoogii when creating offer

--- src/utils/walletIntegration/wallets/hoogiiWallet.tsx
+++ src/utils/walletIntegration/wallets/hoogiiWallet.tsx
@@ -89,13 +89,14 @@ class hoogiiWallet implements WalletIntegrationInterface {
     console.log('Disconnecting Hoogii Wallet')
   }
 
-  async generateOffer(requestAssets: {assetId: string; amount: number;}[], offerAssets: {assetId: string; amount: number;}[]): Promise<void> {
+  async generateOffer(requestAssets: {assetId: string; amount: number;}[], offerAssets: {assetId: string; amount: number;}[], fee: number | undefined): Promise<void> {
     // Hoogii wallet transaction signing logic
     console.log('Generating offer with Hoogii Wallet')
     try {
       const params = {
         requestAssets,
-        offerAssets
+        offerAssets,
+        fee
       }
       const response = await (window as any).chia.hoogii.request({ method: 'createOffer', params })
       console.log('Fetching offer', response)
