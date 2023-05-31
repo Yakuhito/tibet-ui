c9a4a3a6a19cd986d6e2b78edffe8ce996fe0328
Jonathan <jonathanlangton3@gmail.com>
2023-05-31 01:09:10 +0100

    Store wallet chain issues in state & pass state down to ConnectWalletModal to display to user there

--- src/components/walletIntegration/ConnectButton.tsx
+++ src/components/walletIntegration/ConnectButton.tsx
@@ -2,7 +2,6 @@ import ConnectWalletModal from './ConnectedWalletModal';
 import { useState, useContext, useEffect } from 'react';
 import WalletContext from '@/context/WalletContext';
 import { useRouter } from 'next/router';
-import { toast } from 'react-hot-toast';
 import Image from 'next/image';
 
 
@@ -11,35 +10,29 @@ function ConnectButton() {
     const [isWalletModalOpen, setIsWalletModalOpen] = useState(false)
     const { walletManager, activeWallet } = useContext(WalletContext);
     const [address, setAddress] = useState<string | null | void>(null);
+    const [isWalletOnWrongChain, setIsWalletOnWrongChain] = useState(false)
     const router = useRouter();
 
-    // Fetch current wallet address. If address indicates they are on the wrong site (i.e. mainnet / testnet), then redirect them to the correct one
     useEffect(() => {
+      // Fetch current wallet address & save to state
       const fetchAddress = async () => {
         if (activeWallet) {
           const address = await activeWallet.getAddress();
           setAddress(address);
-          //  If users wallet address shows that they are on the wrong chain, redirect them to the right one
-           if (address && address.charAt(0).toLowerCase() === "t" && process.env.NEXT_PUBLIC_XCH === "XCH") {
-            // User should be on testnet site
-            // Redirect to the testnet url
-            toast.error(
-                <p>Your wallet is running testnet. Please switch to mainnet to use this site or use our testnet site <a className="underline hover:opacity-80" href="https://v2.tibetswap.io/" target="_blank">here</a></p>,
-                {duration: 15000}
-            )
-          } else if (address && address.charAt(0).toLowerCase() === "x" && process.env.NEXT_PUBLIC_XCH === "TXCH") {
-            // User should be on mainnet site
-            // Redirect to the mainnet url
-            toast.error(
-                <p>Your wallet is running testnet. Please switch to mainnet to use this site or use our testnet site <a className="underline hover:opacity-80" href="https://v2-testnet10.tibetswap.io/" target="_blank">here</a></p>,
-                {duration: 15000}
-            )
-          }
         }
       };
-  
       fetchAddress();
-    }, [activeWallet, router]);
+
+      //  If users wallet address shows that they are on the wrong chain, display a warning in ConnectedWalletModal
+      if (address && address.charAt(0).toLowerCase() === "t" && process.env.NEXT_PUBLIC_XCH === "XCH") {
+        setIsWalletOnWrongChain(true)
+      } else if (address && address.charAt(0).toLowerCase() === "x" && process.env.NEXT_PUBLIC_XCH === "TXCH") {
+        setIsWalletOnWrongChain(true)
+      } else {
+        setIsWalletOnWrongChain(false)
+      }
+      
+    }, [activeWallet, router, address]);
   
     const displayAddress = () => {
       if (address && process.env.NEXT_PUBLIC_XCH) {
@@ -55,7 +48,7 @@ function ConnectButton() {
                 {activeWallet?.image && <Image src={activeWallet.image} width={20} height={20} alt={`${activeWallet?.name} wallet logo`} className="rounded-full" />}
                 {!activeWallet ? 'Connect Wallet' : displayAddress()}
             </button>
-            <ConnectWalletModal isOpen={isWalletModalOpen} setIsOpen={setIsWalletModalOpen} walletManager={walletManager} activeWallet={activeWallet} />
+            <ConnectWalletModal isOpen={isWalletModalOpen} setIsOpen={setIsWalletModalOpen} walletManager={walletManager} activeWallet={activeWallet} isWalletOnWrongChain={isWalletOnWrongChain} />
         </>
      );
 }
