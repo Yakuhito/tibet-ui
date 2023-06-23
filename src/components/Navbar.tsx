8181d8cb182b8d2f4fc44e286a4376c524e52e08
Jonathan <jonathanlangton3@gmail.com>
2023-06-23 18:27:14 +0100

    Add logo dark mode styling to match previous

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,8 +16,8 @@ export default function Navbar() {
       <header className="sticky w-full top-0 bg-brandLight/50 dark:bg-zinc-900/50 backdrop-blur-xl z-20 md:h-24">
         <div className="container mx-auto px-4 flex gap-4 sm:gap-8 items-center justify-between py-4 h-full">
 
-          <Link href="/">
-            <Logo className="fill-brandDark w-12 h-auto md:w-16 dark:fill-brandLight hover:translate-y-1 hover:opacity-80 transition dark:opacity-80 antialiased" />
+          <Link href="/" className="dark:bg-brandLight p-1 rounded-full w-16 md:w-16 md:h-16 dark:opacity-80 hover:translate-y-1 flex items-center justify-center transition">
+            <Logo className="fill-brandDark max-w-full h-auto mt-1.5" />
           </Link>
 
           {/* Desktop navigation */}
