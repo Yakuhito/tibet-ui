85f581d8527ab0e1a067e2fb8c2bfd09c91dfc4e
Jonathan <jonathanlangton3@gmail.com>
2023-06-23 18:10:26 +0100

    Swapped logo out for SVG version, improved mobile view

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import Logo from './Logo';
 
 export default function Navbar() {
   const router = useRouter();
@@ -12,17 +13,11 @@ export default function Navbar() {
 
   return (
     <>
-      <header className="sticky w-full top-0 bg-brandLight/50 dark:bg-zinc-900/50 backdrop-blur-xl z-20 h-24">
-        <div className="container mx-auto px-4 flex gap-4 sm:gap-8 items-center justify-between py-4">
+      <header className="sticky w-full top-0 bg-brandLight/50 dark:bg-zinc-900/50 backdrop-blur-xl z-20 md:h-24">
+        <div className="container mx-auto px-4 flex gap-4 sm:gap-8 items-center justify-between py-4 h-full">
 
           <Link href="/">
-              <Image
-                src="/logo.jpg"
-                height="64"
-                width="64"
-                alt="TibetSwap Logo"
-                className="rounded-full border-neutral-300 hover:translate-y-1 hover:opacity-80 transition dark:opacity-80"
-              />
+            <Logo className="fill-brandDark w-12 h-auto md:w-16 dark:fill-brandLight hover:translate-y-1 hover:opacity-80 transition dark:opacity-80 antialiased" />
           </Link>
 
           {/* Desktop navigation */}
