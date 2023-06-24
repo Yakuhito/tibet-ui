e4afca321e1b5a33dfd4afb6a8f6c80711410daf
Jonathan <jonathanlangton3@gmail.com>
2023-06-24 15:19:56 +0100

    Fixed mobile dark mode logo sizing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
       <header className="sticky w-full top-0 bg-brandLight/50 dark:bg-zinc-900/50 backdrop-blur-xl z-20 md:h-24">
         <div className="container mx-auto px-4 flex gap-4 sm:gap-8 items-center justify-between py-4 h-full">
 
-          <Link href="/" className="dark:bg-brandLight p-1 rounded-full w-16 md:w-16 md:h-16 dark:opacity-80 hover:translate-y-1 flex items-center justify-center transition">
+          <Link href="/" className="aspect-square dark:bg-brandLight p-1 rounded-full w-12 md:w-16 md:h-16 dark:opacity-80 hover:translate-y-1 flex items-center justify-center transition">
             <Logo className="fill-brandDark max-w-full h-auto mt-1.5" />
           </Link>
 
