79a2d42418d76bf9587ac91c58683924641a7240
Jonathan <jonathanlangton3@gmail.com>
2023-07-12 18:51:31 +0100

    Added fingerprint selector for Chia Wallet option

--- src/components/walletIntegration/ConnectedWalletModal.tsx
+++ src/components/walletIntegration/ConnectedWalletModal.tsx
@@ -4,7 +4,8 @@ import HoogiiWallet from '@/utils/walletIntegration/wallets/hoogiiWallet';
 import GobyWallet from '@/utils/walletIntegration/wallets/gobyWallet';
 import WalletManager from '@/utils/walletIntegration/walletManager';
 import { Dialog, Transition } from '@headlessui/react';
-import { Fragment } from 'react';
+import { Fragment, useState, useEffect } from 'react';
+import { toast } from 'react-hot-toast';
 import Image from 'next/image';
 
 interface ConnectWalletModalProps {
@@ -16,6 +17,23 @@ interface ConnectWalletModalProps {
 }
 
 function ConnectWalletModal({ isOpen, setIsOpen, walletManager, activeWallet,isWalletOnWrongChain }: ConnectWalletModalProps) {
+    const [fingerprints, setFingerprints] = useState([])
+    const [selectedFingerprint, setSelectedFingerprint] = useState<null | number>()
+
+    // Restore Chia Wallet fingerprints from local storage
+    useEffect(() => {
+        const fingerprints_ls = localStorage.getItem("wc_fingerprints")
+        const selectedFingerprint_ls = localStorage.getItem("wc_selectedFingerprint")
+
+        if (fingerprints_ls) setFingerprints(JSON.parse(fingerprints_ls))
+        if (selectedFingerprint_ls) setSelectedFingerprint(JSON.parse(selectedFingerprint_ls))
+    },[])
+
+    const handleSwitchChiaWallet = (fingerprint: number) => {
+        setSelectedFingerprint(fingerprint);
+        localStorage.setItem("wc_selectedFingerprint", JSON.stringify(fingerprint))
+        toast.success(<p className="break-words max-w-[18rem]">Switched to wallet fingerprint <span className="font-mono bg-brandDark/10 text-brandDark/90 px-1 rounded-sm">{fingerprint}</span></p>)
+    }
 
     // Handle connecting to a wallet when clicking on an option
     const handleConnect = async (walletIdentifier: string) => {
@@ -81,17 +99,30 @@ function ConnectWalletModal({ isOpen, setIsOpen, walletManager, activeWallet,isW
 
                     {/* Wallet Options */}
                     <div className="mt-10 flex flex-col gap-4">
+
                         {/* Chia Wallet */}
-                        <div onClick={() => handleConnect('WalletConnect')} className={`${activeWallet instanceof WalletConnect ? 'bg-green-700/20 focus:ring-green-700/20' : 'bg-brandDark/10'} hover:opacity-80 group flex items-center justify-between border-2 border-transparent hover:border-brandDark/10 py-4 px-4 rounded-xl cursor-pointer`}>
-                        <div className="flex items-center gap-4">
-                                <Image src="/assets/xch.webp" height={40} width={40} alt={'Chia Wallet Logo'} className="rounded-full" />
-                                <p className="font-medium text-lg">Chia Wallet</p>
+                        <div>
+                            <div onClick={() => handleConnect('WalletConnect')} className={`${activeWallet instanceof WalletConnect ? 'bg-green-700/20 focus:ring-green-700/20 rounded-t-xl' : 'bg-brandDark/10 rounded-xl'} hover:opacity-80 group flex items-center justify-between border-2 border-transparent hover:border-brandDark/10 py-4 px-4 cursor-pointer`}>
+                                <div className="flex items-center gap-4">
+                                    <Image src="/assets/xch.webp" height={40} width={40} alt={'Chia Wallet Logo'} className="rounded-full" />
+                                    <p className="font-medium text-lg">Chia Wallet</p>
+                                </div>
+                                <button className={`
+                                ${activeWallet instanceof WalletConnect ? 'outline-none text-green-700' : ''}
+                                font-medium rounded-lg px-2 py-1
+                                ${activeWallet instanceof WalletConnect ? "before:content-['Connected']" : "before:content-['Connect']"}`}
+                                ></button>
                             </div>
-                            <button className={`
-                            ${activeWallet instanceof WalletConnect ? 'outline-none text-green-700' : ''}
-                            font-medium rounded-lg px-2 py-1
-                            ${activeWallet instanceof WalletConnect ? "before:content-['Connected']" : "before:content-['Connect']"}`}
-                            ></button>
+                            <div className="animate-fadeIn text-sm bg-brandDark/10 font-medium px-4 py-4 rounded-b-xl flex flex-col gap-2 border-2 border-transparent hover:border-brandDark/10">
+                                <p className="text-base">Your Wallets</p>
+                                <ul className="flex">
+                                {
+                                    fingerprints.map(fingerprint => (
+                                        <li onClick={() => handleSwitchChiaWallet(fingerprint)} className={`select-none rounded-full px-4 py-1 ${fingerprint == selectedFingerprint ? 'bg-green-700/20 focus:ring-green-700/20 text-green-700' : 'cursor-pointer hover:opacity-80'}`} key={fingerprint}>{fingerprint}</li>
+                                    ))
+                                }
+                                </ul>
+                            </div>
                         </div>
                         
                         {/* Goby Wallet */}
