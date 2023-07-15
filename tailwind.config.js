c5dae70df18981007c87a33cfd5c364131fc43e9
Jonathan <jonathanlangton3@gmail.com>
2023-07-15 13:01:00 +0100

    Add src/utils to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/utils/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
