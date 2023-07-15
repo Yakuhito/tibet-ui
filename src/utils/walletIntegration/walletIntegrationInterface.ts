9f26523615eb962ee3d2f172b2fe26527f905b0e
Jonathan <jonathanlangton3@gmail.com>
2023-07-15 13:40:26 +0100

    Updated to include additional data passed to generateOffer

--- src/utils/walletIntegration/walletIntegrationInterface.ts
+++ src/utils/walletIntegration/walletIntegrationInterface.ts
@@ -1,9 +1,28 @@
+export interface generateOffer {
+    requestAssets: {
+      assetId: string
+      amount: number
+      image_url: string
+      short_name: string
+      name: string
+      walletId?: number
+    }[]
+    offerAssets: {
+      assetId: string
+      amount: number
+      image_url: string
+      short_name: string
+      name: string
+      walletId?: number
+    }[]
+  }
+
 interface WalletIntegrationInterface {
     name: string;
     image: string;
     connect(): void;
     disconnect(): void;
-    generateOffer(requestAssets: {assetId: string; amount: number;}[], offerAssets: {assetId: string; amount: number;}[], fee: number | undefined): Promise<string | void>;
+    generateOffer(requestAssets: generateOffer["requestAssets"], offerAssets: generateOffer["offerAssets"], fee: number | undefined): Promise<string | void>;
     getBalance(): void;
     addAsset(assetId: string, symbol: string, logo: string, fullName: string): Promise<void>;
     getAddress(): Promise<string | void>;
