5c12fc0a9b412a8c65f9acd74f3838a9b1f2b258
Jonathan <jonathanlangton3@gmail.com>
2023-07-15 23:53:31 +0100

    Added modal steps (0, 1, 1.5, 2) & improved UI

--- src/utils/walletIntegration/WalletConnect/CompleteWithWalletModal.tsx
+++ src/utils/walletIntegration/WalletConnect/CompleteWithWalletModal.tsx
@@ -19,31 +19,60 @@ const CompleteWithWalletModal = ({ walletConnectInstance, onClose }: CompleteWit
 
   const [isOpen, setIsOpen] = React.useState(true);
   const [step, setStep] = React.useState<number>(0);
-  const [isAddAssetsRequired, setIsAddAssetsRequired] = React.useState(false)
-  const [userMustAddTheseAssetsToWallet, setUserMustAddTheseAssetsToWallet] = React.useState<generateOffer["offerAssets"]>()
-  const [title, setTitle] = React.useState("Fetch Wallets")
+  const [addedAssetsCount, setAddedAssetsCount] = React.useState<number>(0);
+  const [userMustAddTheseAssetsToWallet, setUserMustAddTheseAssetsToWallet] = React.useState<generateOffer["offerAssets"]>([]);
+  const [title, setTitle] = React.useState("Fetch Wallets");
+  const [offerRejected, setOfferRejected] = React.useState(false);
 
   React.useEffect(() => {
     // Assign callback functions from class to component functions
     if (walletConnectInstance) {
-      walletConnectInstance.setOnGetWalletsAccept(handleGetWalletsAccept);
       walletConnectInstance.setOnAddAssets(handleOnAddAssets);
+      walletConnectInstance.setOnGenerateOfferSuccess(handleOnGenerateOfferSuccess)
+      walletConnectInstance.setOnGenerateOfferReject(handleOnGenerateOfferReject)
     }
   }, [walletConnectInstance]);
 
-  // Callback function to update UI modal based on abstract class events
-  const handleGetWalletsAccept = () => {
-    setStep(1);
+  // Callback when everything is good :) The modal kills itself though :'(
+  const handleOnGenerateOfferSuccess = () => {
+    setIsOpen(false);
+    onClose();
   };
 
+  // Callback from class that is run if user needs to add assets to their wallet before continuing
   const handleOnAddAssets = (userMustAddTheseAssetsToWallet: generateOffer["offerAssets"]) => {
-    setUserMustAddTheseAssetsToWallet(userMustAddTheseAssetsToWallet)
-    setIsAddAssetsRequired(true);
-    setTitle("Add Assets")
+    if (userMustAddTheseAssetsToWallet.length) {
+      setUserMustAddTheseAssetsToWallet(userMustAddTheseAssetsToWallet)
+      setTitle("Add Assets")
+      setStep(1);
+    } else {
+      setTitle("Generate Offer")
+      setStep(2);
+    }
   };
+
+  // When user adds an asset in their wallet successfully, add count to added list
+  const handleAssetAdded = () => {
+    setAddedAssetsCount(prevCount => prevCount + 1);
+  }
+
+  // When offer is rejected in Chia wallet, update state to display red cross to user
+  const handleOnGenerateOfferReject = () => {
+    setOfferRejected(true);
+  }
+
+  // If all assets have been added, move to next step (& run generateOffer from the top again)
+  React.useEffect(() => {
+    if (step === 1) {
+      if (addedAssetsCount === userMustAddTheseAssetsToWallet.length) {
+        setTitle("Fetch Wallets")
+        setStep(1.5);
+      }
+    }
+  }, [addedAssetsCount, userMustAddTheseAssetsToWallet.length, step]);
 
   const fetchStep = (step: number) => {
-    if (step === 0) {
+    if (step === 0 || step === 1.5) {
         return (
             <div>
                 <div className="flex gap-4 items-center">
@@ -58,43 +87,48 @@ const CompleteWithWalletModal = ({ walletConnectInstance, onClose }: CompleteWit
         )
     }
     if (step === 1) {
-        if (isAddAssetsRequired) {
-          return (
-            <div>
-                <p>Add the below assets to your Chia Wallet before continuing</p>
-                <ul className="list-none mt-4 flex flex-col gap-2 font-medium">
-                  {userMustAddTheseAssetsToWallet?.map((asset) => {
-                    return (
-                      <li key={asset.assetId} className="flex items-center gap-2 justify-between pb-2 last:pb-0">
-                        <div className="flex gap-4 items-center">
-                            <Image src={asset.image_url} width={30} height={30} alt="Token logo" className="rounded-full outline-brandDark/20 p-0.5" priority />
-                            {!asset.short_name.includes("TIBET-") && <p>{asset.name} ({asset.short_name})</p>}
-                            {asset.short_name.includes("TIBET-") && <p>{asset.short_name}</p>}
-                        </div>
-                        <AddAssetButton asset_id={asset.assetId} short_name={asset.short_name} image_url={asset.image_url} name={asset.name} activeWallet={walletConnectInstance} />
-                        {/* <button className="hover:opacity-80 bg-brandDark/10 py-1 px-4 whitespace-nowrap rounded-lg flex items-center gap-2" onClick={() => addAssetToWallet(asset.assetId, asset.short_name, asset.image_url, asset.name)}>Add to Wallet</button> */}
-                      </li>
-                    )
-                  })}
-                </ul>
-
-                <div className="flex gap-2 mt-8 font-medium">
-                    <button className="bg-red-700 text-brandLight px-4 py-2 rounded-lg focus:outline-none w-full transition duration-100" onClick={() => setIsOpen(false)}>Cancel</button>
-                    {/* <button className="bg-green-800 text-brandLight px-4 py-2 rounded-lg focus:outline-none w-full transition opacity-30 cursor-not-allowed duration-100" onClick={() => setStep(prev => prev+1)}>Next Step</button> */}
-                </div>
+      return (
+        <div>
+            <p>Add the below assets to your Chia Wallet before continuing</p>
+            <ul className="list-none mt-4 flex flex-col gap-2 font-medium">
+              {userMustAddTheseAssetsToWallet?.map((asset) => {
+                return (
+                  <li key={asset.assetId} className="flex items-center gap-2 justify-between pb-2 last:pb-0">
+                    <div className="flex gap-4 items-center">
+                        <Image src={asset.image_url} width={30} height={30} alt="Token logo" className="rounded-full outline-brandDark/20 p-0.5" priority />
+                        {!asset.short_name.includes("TIBET-") && <p>{asset.name} ({asset.short_name})</p>}
+                        {asset.short_name.includes("TIBET-") && <p>{asset.short_name}</p>}
+                    </div>
+                    <AddAssetButton asset_id={asset.assetId} short_name={asset.short_name} image_url={asset.image_url} name={asset.name} activeWallet={walletConnectInstance} onCompletion={handleAssetAdded} />
+                  </li>
+                )
+              })}
+            </ul>
+            
+            <div className="flex gap-2 mt-8 font-medium">
+                <button className="bg-red-700 text-brandLight px-4 py-2 rounded-lg focus:outline-none w-full transition duration-100" onClick={() => setIsOpen(false)}>Cancel</button>
             </div>
-          )
-        } else {
-          return (
-            <div>
-                <p>Approve offer</p>
-                <div className="flex gap-2 mt-8 font-medium">
-                    <button className="bg-red-700 text-brandLight px-4 py-2 rounded-lg focus:outline-none w-full transition duration-100" onClick={() => setIsOpen(false)}>Cancel</button>
-                    {/* <button className="bg-green-800 text-brandLight px-4 py-2 rounded-lg focus:outline-none w-full transition duration-100" onClick={() => setStep(prev => prev+1)}>Next Step</button> */}
-                </div>
-            </div>
-          )
-        }
+        </div>
+      )
+    }
+    if (step === 2) {
+      return (
+        <div>
+          <div className="flex gap-4 items-center">
+            {offerRejected && (
+              <svg className="fill-red-700 max-w-[18px] w-8 aspect-square" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 490 490" xmlSpace="preserve">
+                <polygon points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 489.292,457.678 277.331,245.004 489.292,32.337 "/>
+              </svg>
+            )}
+            {/* Pending Spinner */}
+            {!offerRejected && <div className="max-w-[18px] w-8 aspect-square border-2 border-black/10 rounded-full border-r-black dark:border-r-brandLight animate-spin"></div>}
+            <p>{offerRejected ? 'Offer request rejected. Please cancel and try again.' : "We've sent a request to generate the offer. Please review and accept this in your wallet."}</p>
+          </div>
+          <div className="flex gap-2 mt-8 font-medium">
+            <button className="bg-red-700 text-brandLight px-4 py-2 rounded-lg focus:outline-none w-full transition duration-100" onClick={() => setIsOpen(false)}>Cancel</button>
+          </div>
+      </div>
+      )
     }
 }
 
@@ -127,11 +161,22 @@ const CompleteWithWalletModal = ({ walletConnectInstance, onClose }: CompleteWit
                 >
                     <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white dark:bg-zinc-900 p-6 text-left align-middle shadow-xl transition-all">
 
-                    <Dialog.Title as="h3" className="text-[2.5rem] sm:text-5xl pt-4 pb-4 font-bold text-black dark:text-brandLight">{title}</Dialog.Title>
+                    <Dialog.Title as="h3" className="text-[2.5rem] sm:text-5xl pt-0 pb-4 font-bold text-black dark:text-brandLight">
+                      <ul className="flex gap-2 mb-8">
+                        <li className={`${step >= 0 ? 'bg-brandDark' : 'bg-brandDark/10'} w-full h-1 rounded-full`}></li>
+                        <ul className="w-full flex">
+                          <li className={`${step >= 1 ? 'bg-brandDark' : 'bg-brandDark/10'} w-full h-1 rounded-l-full`}></li>
+                          <li className={`${step >= 1.5 ? 'bg-brandDark' : 'bg-brandDark/10'} w-full h-1 rounded-r-full`}></li>
+                        </ul>
+                        <li className={`${step >= 2 ? 'bg-brandDark' : 'bg-brandDark/10'} w-full h-1 rounded-full`}></li>
+                      </ul>
+                      
+                      {title}
+                    </Dialog.Title>
 
                     {/* Wallet Options */}
                     <div className="mt-4 flex flex-col gap-4">
-                    {fetchStep(step)}
+                      {fetchStep(step)}
                     </div>
                     </Dialog.Panel>
                 </Transition.Child>
