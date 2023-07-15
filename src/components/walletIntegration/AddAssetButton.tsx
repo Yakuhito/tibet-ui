8e360c45c7abc918565c4db304824396ca215ef1
Jonathan <jonathanlangton3@gmail.com>
2023-07-15 23:51:12 +0100

    Added callback when asset has been added

--- src/components/walletIntegration/AddAssetButton.tsx
+++ src/components/walletIntegration/AddAssetButton.tsx
@@ -10,9 +10,10 @@ interface AddAssetButtonProps {
     image_url: string
     name: string
     activeWallet: WalletIntegrationInterface | null
+    onCompletion?: () => void
 }
 
-function AddAssetButton({ asset_id, short_name, image_url, name, activeWallet }: AddAssetButtonProps) {
+function AddAssetButton({ asset_id, short_name, image_url, name, activeWallet, onCompletion = () => {} }: AddAssetButtonProps) {
 
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [isAdded, setIsAdded] = useState<boolean>(false);
@@ -22,13 +23,14 @@ function AddAssetButton({ asset_id, short_name, image_url, name, activeWallet }:
         if (!activeWallet) return toast.error('Connect to a wallet before trying to add an asset');
         const response = await activeWallet.addAsset(assetId, symbol, logo, fullName);
         if (response) {
-            setIsAdded(true)
+            setIsAdded(true);
+            onCompletion();
         }
         setIsLoading(false);
     }
 
     return ( 
-        <button onClick={() => addAssetToWallet(asset_id, short_name, image_url, name)} className={`${isLoading ? 'animate-pulse' : ''} ${isAdded ? 'bg-green-700/20 text-green-700' : 'bg-brandDark/10 text-brandDark'} h-fit w-40 hover:opacity-80 py-1 px-4 whitespace-nowrap rounded-lg flex justify-center items-center gap-3`}>
+        <button onClick={() => addAssetToWallet(asset_id, short_name, image_url, name)} className={`${isLoading ? 'animate-pulse' : ''} ${isAdded ? 'bg-green-700/20 text-green-700' : 'bg-brandDark/10 text-brandDark dark:text-brandLight'} h-fit max-w-[160px] w-full hover:opacity-80 py-1 px-4 whitespace-nowrap rounded-lg flex justify-center items-center gap-3`}>
             {!isLoading && !isAdded && <Image src={activeWallet instanceof GobyWallet ? "/assets/goby.webp" : "/assets/xch.webp"} width={16} height={16} alt="Token logo" className="rounded-full" />}
             {isLoading && <div className="w-4 aspect-square border border-black/10 rounded-full border-r-brandDark dark:border-r-brandLight animate-spin"></div>}
             {isAdded ? 'Added' : 'Add to Wallet'}
