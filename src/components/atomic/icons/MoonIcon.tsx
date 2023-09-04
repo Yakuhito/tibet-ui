46e913631077b95aed37d25e20ad7bc1c1de834a
Jonathan <jonathanlangton3@gmail.com>
2023-09-04 11:27:28 +0100

    Extracted sun & moon svg to icon components

---
+++ src/components/atomic/icons/MoonIcon.tsx
@@ -0,0 +1,24 @@
+type MoonIconProps = {
+  className?: string;
+}
+
+function MoonIcon({ className }: MoonIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+    </svg>
+  );
+}
+
+export default MoonIcon;
