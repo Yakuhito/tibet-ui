4418307172abcdd0bda8c4983284f58ba1d2cc0d
Jonathan <jonathanlangton3@gmail.com>
2023-09-13 13:12:33 +0100

    Polished fingerprint management & persistence

--- src/components/walletIntegration/WalletConnectSession.tsx
+++ src/components/walletIntegration/WalletConnectSession.tsx
@@ -1,12 +1,13 @@
-import { disconnectSession, selectSession } from '@/redux/walletConnectSlice';
+import { disconnectSession, selectSession, setSelectedFingerprint } from '@/redux/walletConnectSlice';
 import { connectWallet } from '@/redux/walletSlice';
+import { useState, useEffect } from 'react';
 import CrossIcon from '../icons/CrossIcon';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 import { useAppDispatch } from '@/hooks';
-import { useState } from 'react';
 import Image from 'next/image';
 
+import FingerprintListbox from './FingerprintListbox';
 
 interface WalletConnectSession {
   img: string;
@@ -22,9 +23,34 @@ function WalletConnectSession({ img, name, topic }: WalletConnectSession) {
   const connectedWallet = useSelector((state: RootState) => state.wallet.connectedWallet);
   const sessions = useSelector((state: RootState) => state.walletConnect.sessions);
   const selectedSession = useSelector((state: RootState) => state.walletConnect.selectedSession);
-  const selectedFingerprint = useSelector((state: RootState) => state.walletConnect.selectedFingerprint);
+  const selectedFingerprint = useSelector((state: RootState) => state.walletConnect.selectedFingerprint)[topic];
   const isSelected = selectedSession ? selectedSession.topic === topic : false;
+  
+  
+  // Fingerprint management
+  const session = sessions.find(session => session.topic === topic);
+  const defaultFingerprint = session ? Number(session.namespaces.chia.accounts[0].split(":")[2]) : 0;
+  const [userSelectedFingerprint, setUserSelectedFingerprint] = useState<number | undefined>(selectedFingerprint ? selectedFingerprint : defaultFingerprint);
+  const [fingerprints, setFingerprints] = useState<number[] | undefined>()
 
+  // Set fingerprints array
+  useEffect(() => {
+    if (session) {
+      const fingerprints = session.namespaces.chia.accounts.map(wallet => {
+        return Number(wallet.split(":")[2]);
+      });
+      setFingerprints(fingerprints);
+    }
+  }, [session]);
+  
+  // When user updates the selected fingerprint, tell the Redux store to save that preference for the session topic
+  useEffect(() => {
+    if (userSelectedFingerprint) {
+      dispatch(setSelectedFingerprint({ topic, selectedFingerprint: userSelectedFingerprint }));
+    }
+  }, [userSelectedFingerprint, dispatch, topic])
+
+  
   const isConnectedWalletWalletConnect = useSelector((state: RootState) => state.wallet.connectedWallet) === "WalletConnect";
 
   const handleClick = () => {
@@ -34,23 +60,6 @@ function WalletConnectSession({ img, name, topic }: WalletConnectSession) {
     dispatch(selectSession(topic));
   }
 
-  const displayFingerprints = () => {
-    const session = sessions.find(session => session.topic === topic);
-    if (sessions.length && session && selectedSession) {
-      const fingerprints = session.namespaces.chia.accounts.map(wallet => {
-        return Number(wallet.split(":")[2]);
-      });
-      const isActiveWallet = selectedSession.topic === topic;
-      return fingerprints.map(fingerprint => {
-        const isSelectedFingerprint = selectedFingerprint == fingerprint;
-        return (
-          <li className={`${isSelectedFingerprint && isActiveWallet && connectedWallet === "WalletConnect" ? "bg-green-700 text-brandLight" : ""} px-2 rounded-full`} key={fingerprint}>{fingerprint}</li>
-        )
-      })
-    }
-    return <></>
-  }
-
   return ( 
     <li onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)} onClick={handleClick} className={`${isSelected && isConnectedWalletWalletConnect ? 'bg-green-700/20 text-green-700' : 'bg-brandDark/10'} ${isHovering && !isSelected ? 'bg-green-700/10 text-green-700': ''} select-none rounded-xl px-8 py-4 cursor-pointer hover:opacity-80 flex justify-between items-center w-full h-16 animate-fadeIn`}>
       <div className="flex gap-4 items-center">
@@ -58,7 +67,7 @@ function WalletConnectSession({ img, name, topic }: WalletConnectSession) {
         <div className="flex flex-col">
           {isHovering && !isSelected ? "Select" : name}
           <ul className="flex gap-1 text-xs">
-            {displayFingerprints()}
+            <FingerprintListbox fingerprints={fingerprints} userSelectedFingerprint={userSelectedFingerprint} setUserSelectedFingerprint={setUserSelectedFingerprint}  />
           </ul>
         </div>
       </div>
