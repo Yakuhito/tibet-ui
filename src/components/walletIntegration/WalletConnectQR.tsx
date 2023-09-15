b74917cdba9f5d40c09b28c2b22577cc332235a9
Jonathan <jonathanlangton3@gmail.com>
2023-09-15 09:43:49 +0100

    New wallet connect QR component using Redux

---
+++ src/components/walletIntegration/WalletConnectQR.tsx
@@ -0,0 +1,30 @@
+import { setPairingUri } from '@/redux/walletConnectSlice';
+import CopyButton from '../atomic/CopyButton';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+import { QRCodeSVG } from 'qrcode.react';
+import { useAppDispatch } from '@/hooks';
+
+function WalletConnectQR() {
+
+  const dispatch = useAppDispatch();
+
+  const pairingUri = useSelector((state: RootState) => state.walletConnect.pairingUri);
+  if (!pairingUri) return <></>
+
+  return ( 
+    <>
+      <QRCodeSVG
+        value={pairingUri}
+        includeMargin
+        width={"100%"}
+        height={200}
+        bgColor="transparent"
+      />
+      <button className="text-red-700" onClick={() => dispatch(setPairingUri(null))}>Cancel</button>
+      <CopyButton copyText={pairingUri} height="36px">Copy pairing string</CopyButton>
+    </>    
+  );
+}
+
+export default WalletConnectQR;
