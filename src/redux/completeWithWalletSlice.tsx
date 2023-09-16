fd5bf22702eadd9a35864feb3412ed641fc8e090
Jonathan <jonathanlangton3@gmail.com>
2023-09-16 13:42:47 +0100

    Extracted transaction properties to non-persisted slice

---
+++ src/redux/completeWithWalletSlice.tsx
@@ -0,0 +1,39 @@
+import { generateOffer } from '@/utils/walletIntegration/walletIntegrationInterface';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface completeWithWalletSliceState {
+  requestStep: null | "getWallets" | "addAssets" | "getWalletsAgain" | "generateOffer";
+  userMustAddTheseAssetsToWallet: generateOffer["offerAssets"];
+  offerRejected: boolean;
+}
+
+// SLICES
+/////////////////////////////////
+const initialState: completeWithWalletSliceState = {
+  requestStep: null,
+  userMustAddTheseAssetsToWallet: [],
+  offerRejected: false
+};
+
+const completeWithWalletSlice = createSlice({
+  name: 'completeWithWallet',
+  initialState,
+  reducers: {
+    setRequestStep(state, action: PayloadAction<null | "getWallets" | "addAssets" | "getWalletsAgain" | "generateOffer">) {
+      const newStep = action.payload;
+      state.requestStep = newStep;
+    },
+
+    setUserMustAddTheseAssetsToWallet(state, action: PayloadAction<[] | generateOffer["offerAssets"]>) {
+      state.userMustAddTheseAssetsToWallet = action.payload;
+    },
+
+    setOfferRejected(state, action: PayloadAction<boolean>) {
+      state.offerRejected = action.payload;
+    }
+  },
+});
+
+export const { setRequestStep, setUserMustAddTheseAssetsToWallet, setOfferRejected } = completeWithWalletSlice.actions;
+
+export default completeWithWalletSlice.reducer;
