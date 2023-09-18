78b6108f27273c64ece63df0b7312d2933140010
Jonathan <jonathanlangton3@gmail.com>
2023-09-18 09:35:43 +0100

    Handle edge-case making WC display as connected, but with no active session

--- src/components/walletIntegration/ConnectButton.tsx
+++ src/components/walletIntegration/ConnectButton.tsx
@@ -1,11 +1,15 @@
+import { selectSession } from '@/redux/walletConnectSlice';
 import ConnectWalletModal from './ConnectedWalletModal';
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
+import { useAppDispatch } from '@/hooks';
 import Image from 'next/image';
 
 
 function ConnectButton() {
+
+    const dispatch = useAppDispatch();
 
     const [isWalletModalOpen, setIsWalletModalOpen] = useState(false)
     const [isWalletOnWrongChain, setIsWalletOnWrongChain] = useState(false)
@@ -15,9 +19,13 @@ function ConnectButton() {
     const walletImage = useSelector((state: RootState) => state.wallet.image);
     const walletName = useSelector((state: RootState) => state.wallet.name);
     const walletConnectSelectedSession = useSelector((state: RootState) => state.walletConnect.selectedSession);
+    const walletConnectSessions = useSelector((state: RootState) => state.walletConnect.sessions);
     const displayWalletImage = (() => {
       if (walletName === "WalletConnect" && walletConnectSelectedSession) {
         return walletConnectSelectedSession.peer.metadata.icons[0];
+      } else if (connectedWallet === "WalletConnect" && !walletConnectSelectedSession && walletConnectSessions.length) {
+        dispatch(selectSession(walletConnectSessions[0].topic));
+        return walletConnectSessions[0].peer.metadata.icons[0];
       } else {
         return walletImage;
       }
@@ -47,8 +55,8 @@ function ConnectButton() {
     return ( 
         <>
             <button onClick={() => setIsWalletModalOpen(true)} className="flex items-center gap-2 bg-brandDark/10  text-brandDark dark:text-brandLight px-6 py-1.5 font-medium rounded-xl animate-fadeIn hover:opacity-80">
-                {connectedWallet && displayWalletImage && <Image src={displayWalletImage} width={20} height={20} alt={`${walletName} wallet logo`} className="rounded-full" />}
-                {!connectedWallet ? 'Connect Wallet' : displayAddress()}
+                {(connectedWallet && displayWalletImage) && <Image src={displayWalletImage} width={20} height={20} alt={`${walletName} wallet logo`} className="rounded-full" />}
+                {!connectedWallet || (connectedWallet === "WalletConnect" && !walletConnectSelectedSession) ? 'Connect Wallet' : displayAddress()}
             </button>
             <ConnectWalletModal isOpen={isWalletModalOpen} setIsOpen={setIsWalletModalOpen} isWalletOnWrongChain={isWalletOnWrongChain} />
         </>
