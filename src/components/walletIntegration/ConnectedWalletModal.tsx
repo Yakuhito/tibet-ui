922fca0753ee559706b2cac74d5ddd20a864b82f
Jonathan <jonathanlangton3@gmail.com>
2023-09-18 09:36:21 +0100

    Handle edge-case making WC display as connected, but with no active session

--- src/components/walletIntegration/ConnectedWalletModal.tsx
+++ src/components/walletIntegration/ConnectedWalletModal.tsx
@@ -26,13 +26,13 @@ function ConnectWalletModal({ isOpen, setIsOpen, isWalletOnWrongChain }: Connect
     const dispatch = useAppDispatch();
     const connectedWallet = useSelector((state: RootState) => state.wallet.connectedWallet);
     const walletConnectSessions = useSelector((state: RootState) => state.walletConnect.sessions);
-
+    const walletConnectSelectedSession = useSelector((state: RootState) => state.walletConnect.selectedSession);
     
     useEffect(() => {
         dispatch(getAllSessions());
     }, [dispatch])
 
-    const chiaActive = connectedWallet === "WalletConnect";
+    const walletConnectActive = connectedWallet === "WalletConnect" && walletConnectSelectedSession;
     const gobyActive = connectedWallet === "Goby";
     const hoogiiActive = connectedWallet === "Hoogii";
 
@@ -74,21 +74,18 @@ function ConnectWalletModal({ isOpen, setIsOpen, isWalletOnWrongChain }: Connect
 
                         {/* Wallet Connect */}
                         <div>
-                            <div onClick={() => walletConnectSessions.length ? dispatch(connectWallet("WalletConnect")) : dispatch(connectSession())} className={`${chiaActive ? `bg-green-700/20 focus:ring-green-700/20` : 'bg-brandDark/10'} ${isWalletConnectOpen || chiaActive ? 'rounded-t-xl' : 'rounded-xl'} hover:opacity-80 group flex items-center justify-between border-2 border-transparent hover:border-brandDark/10 py-4 px-4 cursor-pointer`}>
+                            <div onClick={() => walletConnectSessions.length ? dispatch(connectWallet("WalletConnect")) : dispatch(connectSession())} className={`${walletConnectActive ? `bg-green-700/20 focus:ring-green-700/20` : 'bg-brandDark/10'} ${isWalletConnectOpen || walletConnectActive ? 'rounded-t-xl' : 'rounded-xl'} hover:opacity-80 group flex items-center justify-between border-2 border-transparent hover:border-brandDark/10 py-4 px-4 cursor-pointer`}>
                                 <div className="flex items-center gap-4">
                                     <WalletConnectIcon className="w-10 h-10" />
                                     <p className="font-medium text-lg">Wallet Connect</p>
                                 </div>
                                 <button className={`
-                                ${chiaActive ? 'outline-none text-green-700' : ''}
+                                ${walletConnectActive ? 'outline-none text-green-700' : ''}
                                 font-medium rounded-lg px-2 py-1
-                                ${chiaActive ? "before:content-['Connected']" : "before:content-['Connect']"}`}
+                                ${walletConnectActive ? "before:content-['Connected']" : "before:content-['Connect']"}`}
                                 ></button>
                             </div>
-                            {(isWalletConnectOpen || chiaActive) && 
-
-
-
+                            {(isWalletConnectOpen || walletConnectActive) ? 
 
                             <div className="animate-fadeIn text-sm bg-brandDark/10 font-medium px-4 py-4 rounded-b-xl flex flex-col gap-2 border-2 border-transparent hover:border-brandDark/10">
                                 <p className="text-base">Sessions</p>
@@ -108,7 +105,7 @@ function ConnectWalletModal({ isOpen, setIsOpen, isWalletOnWrongChain }: Connect
                                     </ul>
                                 )}
                             </div>
-                            
+                            : <></>
                             }
                         </div>
 
