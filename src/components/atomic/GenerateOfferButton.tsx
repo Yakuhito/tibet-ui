9ec69c703c79ca077338a5a5c024fd91b2b4004d
Jonathan <jonathanlangton3@gmail.com>
2023-09-22 13:20:15 +0100

    Removed fade in & transitions. Reduced font size.

--- src/components/atomic/GenerateOfferButton.tsx
+++ src/components/atomic/GenerateOfferButton.tsx
@@ -11,7 +11,7 @@ const GenerateOfferButton: React.FC<GenerateOfferButtonProps> = ({ disabled, onP
     <button
       onClick={onPressed}
       className={`${disabled ? "bg-brandDark/10 text-brandDark/20 dark:text-brandLight/30 cursor-not-allowed" : "hover:opacity-90"} 
-        p-4 rounded-xl w-full mt-8 h-[72px] font-bold bg-gradient-to-br from-[#7fa9b8]/90 to-brandDark/90 dark:from-brandDark dark:to-[#152f38] text-brandLight text-2xl animate-fadeIn`}
+        p-4 rounded-xl w-full mt-8 font-medium bg-gradient-to-br from-[#7fa9b8]/90 to-brandDark/90 dark:from-brandDark dark:to-[#152f38] text-brandLight text-xl`}
       disabled={disabled}
     >
       {text}
