f0bb02521e17c269db7cd7bf6498aa1ec5d1a6f3
Jonathan <jonathanlangton3@gmail.com>
2023-09-22 13:10:38 +0100

    Pass down selectToken prop & small UI tweaks

--- src/components/LiquidityInput.tsx
+++ src/components/LiquidityInput.tsx
@@ -1,6 +1,6 @@
 import AssetAmountInput from "@/components/atomic/AssetAmountInput";
 import ChevronDownIcon from "@/components/icons/ChevronDownIcon";
-import { Token } from "../api";
+import type { Token } from "../api";
 
 type LiquidityInputsProps = {
   token0: Token;
@@ -13,6 +13,7 @@ type LiquidityInputsProps = {
   amount2: number;
   onAmountsChanged: (amount0: number, amount1: number, amount2: number) => void;
   disabled: boolean;
+  selectToken: (token: Token) => void;
 };
 
 const LiquidityInput: React.FC<LiquidityInputsProps> = ({
@@ -26,16 +27,20 @@ const LiquidityInput: React.FC<LiquidityInputsProps> = ({
   onAmountsChanged,
   onArrowClick,
   disabled,
+  selectToken,
 }) => {
   return (
     <div className="w-fill mt-8">
+
       <AssetAmountInput
         token={token0}
         value={amount0}
         onChange={(val) => onAmountsChanged(val, amount1, amount2)}
         maxDecimals={12}
         disabled={true}
+        selectToken={selectToken}
       />
+
       <div className="mt-2">
         <AssetAmountInput
           token={token1}
@@ -43,29 +48,26 @@ const LiquidityInput: React.FC<LiquidityInputsProps> = ({
           onChange={(val) => onAmountsChanged(amount0, val, amount2)}
           maxDecimals={3}
           disabled={disabled || !isAddSelected}
+          selectToken={selectToken}
         />
       </div>
+
       <div
-        className="p-1 items-center justify-center flex w-fill"
-        onClick={disabled ? () => {} : onArrowClick}
-      >
-        <ChevronDownIcon
-          className={`h-7 w-7 ${
-            disabled
-              ? "text-gray-300 cursor-not-allowed"
-              : isAddSelected
-              ? "text-green-700 cursor-pointer"
-              : "text-red-800 cursor-pointer"
-          } ${isAddSelected ? "" : "transform rotate-180"}`}
-        />
+        className={`bg-slate-100 dark:bg-zinc-900 w-10 h-10 -mt-4 -mb-4 mx-auto rounded-full select-none items-center justify-center flex z-10 relative border-2 border-brandDark/10 
+        ${disabled ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'}`}
+        onClick={disabled ? () => {} : onArrowClick}>
+          <ChevronDownIcon className={`h-4 w-4 ${isAddSelected ? "" : "rotate-180"}`} />
       </div>
+
       <AssetAmountInput
         token={token2}
         value={amount2}
         onChange={(val) => onAmountsChanged(amount0, amount1, val)}
         maxDecimals={3}
         disabled={disabled || isAddSelected}
+        selectToken={selectToken}
       />
+
     </div>
   );
 };
