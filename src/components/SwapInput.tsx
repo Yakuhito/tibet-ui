839b32146863245cc96857505cb57cc871829549
Jonathan <jonathanlangton3@gmail.com>
2023-09-23 13:19:31 +0100

    Added animation when changing between buy/sell

--- src/components/SwapInput.tsx
+++ src/components/SwapInput.tsx
@@ -1,6 +1,7 @@
 import AssetAmountInput from "@/components/atomic/AssetAmountInput";
 import ChevronDownIcon from "@/components/icons/ChevronDownIcon";
 import type { Token } from "../api";
+import { Transition } from "@headlessui/react";
 
 type SwapInputsProps = {
   token0: Token;
@@ -27,14 +28,17 @@ const SwapInputs: React.FC<SwapInputsProps> = ({
 }) => {
   return (
     <div className="w-fill mt-8">
-      <AssetAmountInput
-        token={token0}
-        value={amount0}
-        onChange={(val) => onAmountsChanged(val, amount1)}
-        maxDecimals={12}
-        selectToken={selectToken}
-        disabled={disabled}
-      />
+
+      <div className={`relative h-[102px] transition-transform ${isBuySelected ? 'translate-y-0' : 'translate-y-[calc(100%+0.5rem)]'}`}>
+        <AssetAmountInput
+          token={token0}
+          value={amount0}
+          onChange={(val) => onAmountsChanged(val, amount1)}
+          maxDecimals={12}
+          selectToken={selectToken}
+          disabled={disabled}
+        />
+      </div>
 
       <div
         className={`bg-slate-100 dark:bg-zinc-900 w-10 h-10 -mt-4 -mb-4 mx-auto rounded-full select-none items-center justify-center flex z-10 relative border-2 border-brandDark/10 
@@ -43,14 +47,17 @@ const SwapInputs: React.FC<SwapInputsProps> = ({
           <ChevronDownIcon className={`h-4 w-4 ${isBuySelected ? "" : "rotate-180"}`} />
       </div>
 
-      <AssetAmountInput
-        token={token1}
-        value={amount1}
-        onChange={(val) => onAmountsChanged(amount0, val)}
-        maxDecimals={3}
-        selectToken={selectToken}
-        disabled={disabled}
-      />
+      <div className={`relative h-[102px] transition-transform ${isBuySelected ? 'translate-y-0' : 'translate-y-[calc(-100%-0.5rem)]'}`}>
+        <AssetAmountInput
+          token={token1}
+          value={amount1}
+          onChange={(val) => onAmountsChanged(amount0, val)}
+          maxDecimals={3}
+          selectToken={selectToken}
+          disabled={disabled}
+        />
+      </div>
+
     </div>
   );
 };
