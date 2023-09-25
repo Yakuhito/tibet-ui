c8345a6160db0d4de067a0d03913e9a7b3de7bdb
Jonathan <jonathanlangton3@gmail.com>
2023-09-25 10:34:40 +0100

    Added mobile navigation menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
 import ConnectButton from './walletIntegration/ConnectButton';
+import MobileNavMenuModal from './modals/MobileNavMenuModal';
 import { setIsOpen } from '@/redux/settingsModalSlice';
 import SettingsModal from './modals/SettingsModal';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 import { useAppDispatch } from '@/hooks';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
+import MenuIcon from './icons/MenuIcon';
 import CogIcon from './icons/CogIcon';
 import Logo from './icons/Logo';
 import Link from 'next/link';
-import React from 'react';
 
 interface NavbarProps {
   theme: "dark" | "light" | "auto";
@@ -23,6 +25,8 @@ export default function Navbar({ theme, setTheme }: NavbarProps) {
     dispatch(setIsOpen(value));
   }
 
+  const [isMobileNavModalOpen, setIsMobileNavModalOpen] = useState(false);
+
   return (
     <>
       <header className="sticky w-full top-0 bg-brandLight/50 dark:bg-zinc-900/50 backdrop-blur-xl z-20 md:h-24">
@@ -32,7 +36,7 @@ export default function Navbar({ theme, setTheme }: NavbarProps) {
             <Logo className="fill-brandDark max-w-full h-auto mt-1.5" />
           </Link>
 
-          {/* Desktop navigation */}
+          {/* Desktop Navigation */}
           <nav className="hidden md:flex items-center rounded-xl p-1">
             <Link href="/" className={`font-medium text-brandDark px-6 py-1.5 rounded-xl ${router.asPath === "/" || router.pathname === "/[pair_short_name]" ? 'dark:text-brandLight bg-brandDark/10' : 'text-brandDark/70 dark:text-brandLight/50 hover:opacity-80'}`}>Trade</Link>
             <Link href="/faq" className={`font-medium text-brandDark px-6 py-1.5 rounded-xl ${router.asPath === "/faq" ? 'dark:text-brandLight bg-brandDark/10' : 'text-brandDark/70 dark:text-brandLight/50 hover:opacity-80'}`}>FAQs</Link>
@@ -43,17 +47,13 @@ export default function Navbar({ theme, setTheme }: NavbarProps) {
           <div className="hidden sm:block ">
             <ConnectButton />
           </div>
-
+          
+          {/* Mobile Navigation */}
+          <MenuIcon className="w-8 text-brandDark dark:text-brandLight cursor-pointer hover:opacity-80 sm:hidden" onClick={() => setIsMobileNavModalOpen(true)}  />
+          <MobileNavMenuModal isOpen={isMobileNavModalOpen} setIsOpen={setIsMobileNavModalOpen} />
+          
         </div>
       </header>
-
-      {/* <header className="md:hidden fixed bottom-0 left-0 w-full bg-brandLight/50 dark:bg-zinc-900/50 backdrop-blur-xl z-20 p-4">
-        <nav className="flex items-center rounded-xl p-1 w-full text-center">
-            <Link href="/" className={`w-full font-medium text-brandDark px-6 py-1.5 rounded-xl ${router.asPath === "/" || router.pathname === "/[pair_short_name]" ? 'dark:text-brandLight bg-brandDark/10' : 'text-brandDark/70 dark:text-brandLight/50 hover:opacity-80'}`}>Trade</Link>
-            <Link href="/faq" className={`w-full font-medium text-brandDark px-6 py-1.5 rounded-xl ${router.asPath === "/faq" ? 'dark:text-brandLight bg-brandDark/10' : 'text-brandDark/70 dark:text-brandLight/50 hover:opacity-80'}`}>FAQs</Link>
-            <Link href={`${process.env.NEXT_PUBLIC_INFO_BASE_URL}`} className="w-full font-medium text-brandDark text-brandDark/70 dark:text-brandLight/50 px-6 py-1.5 rounded-xl hover:opacity-80">Stats</Link>
-        </nav>
-      </header> */}
     </>
   );
 };
