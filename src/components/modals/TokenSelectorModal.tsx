b8cc1e4a1592a9c55b2eba14b85b3a385b65a8eb
Jonathan <jonathanlangton3@gmail.com>
2023-09-25 12:32:18 +0100

    Use react-window to only render required tokens to DOM to increase performance

--- src/components/modals/TokenSelectorModal.tsx
+++ src/components/modals/TokenSelectorModal.tsx
@@ -1,10 +1,13 @@
 import { Dialog, Transition } from '@headlessui/react';
-import { ChangeEvent, Fragment, useState } from 'react';
+import { CSSProperties, ChangeEvent, Fragment, useState } from 'react';
 import SearchIcon from '../icons/SearchIcon';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 import type { Token } from '@/api';
 import Image from 'next/image';
+
+import { FixedSizeList as List } from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
 
 interface TokenSelectorProps {
     isOpen: boolean;
@@ -14,24 +17,45 @@ interface TokenSelectorProps {
 
 function TokenSelectorModal({ isOpen, setIsOpen, setSelectedToken }: TokenSelectorProps) {
 
-    const tokens = useSelector((state: RootState) => state.globalOnLoadData.tokens);
-    const [searchTerm, setSearchTerm] = useState<string>("");
+  const tokens = useSelector((state: RootState) => state.globalOnLoadData.tokens);
+  const [searchTerm, setSearchTerm] = useState<string>("");
 
-    const filteredTokens = tokens ? tokens.filter(token => {
-      const searchableName = (`${token.name} ${token.short_name} ${token.asset_id}`).toLowerCase();
-      return searchableName.includes(searchTerm.toLowerCase())
-      }
-    ) : null;
-
-    const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-      setSearchTerm(e.target.value);
+  const filteredTokens = tokens ? tokens.filter(token => {
+    const searchableName = (`${token.name} ${token.short_name} ${token.asset_id}`).toLowerCase();
+    return searchableName.includes(searchTerm.toLowerCase())
     }
+  ) : null;
 
-    const selectToken = (token: Token) => {
-      setIsOpen(false);
-      setSelectedToken(token);
-      setSearchTerm("");
-    }
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  }
+
+  const selectToken = (token: Token) => {
+    setIsOpen(false);
+    setSelectedToken(token);
+    setSearchTerm("");
+  }
+
+  const tokenRow = ({ index, style }: { index: number, style: CSSProperties }) => {
+    if (!filteredTokens) return <p>No tokens</p>
+    const token = filteredTokens[index]
+    return (
+      <li
+        key={token.asset_id}
+        style={style}
+        className="flex gap-6 max-h-full items-center select-none cursor-pointer group hover:bg-brandDark/0 px-4 py-2 rounded-lg"
+        onClick={() => selectToken(token)}
+      >
+        <div className="w-10 h-10 rounded-full relative overflow-hidden">
+          <Image className="absolute top-0 left-0 w-10 h-10 rounded-full animate-fadeIn z-10 bg-brandLight" src={token.image_url} width={100} height={100} alt={token.name} priority />
+          <div className="w-full h-full rounded-full text-brandLight font-medium flex justify-center items-center bg-brandDark/20 backdrop-blur">{token.short_name.substring(0, 2)}</div>
+        </div>
+        <div className="group-hover:pl-0.5 transition-all pl-0">
+          <p className="font-medium text-xl">{token.short_name}</p>
+        </div>
+      </li>
+    )
+  }
 
     return (    
         <Transition appear show={isOpen} as={Fragment}>
@@ -64,7 +88,7 @@ function TokenSelectorModal({ isOpen, setIsOpen, setSelectedToken }: TokenSelect
                     <Dialog.Title as="h3" className="text-[2.5rem] sm:text-5xl pt-4 pb-4 md:pb-8 font-bold text-black dark:text-brandLight">Select a token</Dialog.Title>
 
                     {/* Settings Options */}
-                    <div className="flex flex-col max-h-full">
+                    <div className="flex flex-col max-h-full min-h-full">
 
                       {/* Search */}
                       <div className="relative w-full flex flex-col items-center">
@@ -81,24 +105,29 @@ function TokenSelectorModal({ isOpen, setIsOpen, setSelectedToken }: TokenSelect
                         leaveFrom="opacity-100 translate-y-0 blur"
                         leaveTo="opacity-0 translate-y-10 blur"
                       >
-                        <ul className="flex flex-col gap-3 pt-10 max-h-full h-full overflow-y-auto overflow-x-hidden">
+                        <div className="relative h-[calc(100svh-164px)]">
                           {filteredTokens && (
-                            filteredTokens.map(token => (
-                              <li
-                                key={token.asset_id}
-                                className="flex gap-6 max-h-full items-center select-none cursor-pointer group hover:bg-brandDark/0 px-4 py-2 rounded-lg"
-                                onClick={() => selectToken(token)}
-                              >
-                                <Image className="w-10 h-10 rounded-full animate-fadeIn" src={token.image_url} width={100} height={100} alt={token.name} priority />
-                                <div className="group-hover:pl-0.5 transition-all pl-0">
-                                  <p className="font-medium text-xl">{token.short_name}</p>
-                                  {/* <p className="opacity-50 text-base">{token.name}</p> */}
-                                </div>
-                              </li>
-                            ))
+                            <div className="absolute w-full h-full min-h-full max-h-full left-0 top-0">
+                              <AutoSizer>
+                                {({ height, width }) => (
+                                  <List
+                                    className="List"
+                                    height={height}
+                                    itemCount={filteredTokens.length}
+                                    itemSize={68}
+                                    width={width}
+                                  >
+                                    {tokenRow}
+                                  </List>
+                                )}
+                              </AutoSizer>
+                            </div>
                           )}
-                        </ul>
+                        </div>
+
                       </Transition.Child>
+
+                      
 
 
                     </div>
