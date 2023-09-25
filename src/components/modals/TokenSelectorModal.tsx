0ca09a4a8c1b4d500133cecac70d1faab25528b0
Jonathan <jonathanlangton3@gmail.com>
2023-09-25 12:36:01 +0100

    Fix tab-hell

--- src/components/modals/TokenSelectorModal.tsx
+++ src/components/modals/TokenSelectorModal.tsx
@@ -57,84 +57,86 @@ function TokenSelectorModal({ isOpen, setIsOpen, setSelectedToken }: TokenSelect
     )
   }
 
-    return (    
-        <Transition appear show={isOpen} as={Fragment}>
-            <Dialog as="div" className="relative z-20" onClose={() => setIsOpen(false)}>
+
+  
+  return (    
+    <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-20" onClose={() => setIsOpen(false)}>
+        <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-200"
+            enterFrom="bg-white/20"
+            enterTo="bg-white/80"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 dark:bg-zinc-900/90 backdrop-blur" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full h-full items-center justify-center p-4 pb-0 text-center">
             <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-200"
-                enterFrom="bg-white/20"
-                enterTo="bg-white/80"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
                 leave="ease-in duration-200"
                 leaveFrom="opacity-100"
                 leaveTo="opacity-0"
             >
-              <div className="fixed inset-0 dark:bg-zinc-900/90 backdrop-blur" />
-            </Transition.Child>
-    
-            <div className="fixed inset-0 overflow-y-auto">
-                <div className="flex min-h-full h-full items-center justify-center p-4 pb-0 text-center">
-                <Transition.Child
+                <Dialog.Panel className="w-full max-w-md h-full transform overflow-hidden rounded-2xl p-2 text-left align-middle transition-all">
+
+                <Dialog.Title as="h3" className="text-[2.5rem] sm:text-5xl pt-4 pb-4 md:pb-8 font-bold text-black dark:text-brandLight">Select a token</Dialog.Title>
+
+                {/* Main Content Container */}
+                <div className="flex flex-col max-h-full min-h-full">
+
+                  {/* Search */}
+                  <div className="relative w-full flex flex-col items-center">
+                    {/* <SearchIcon className="h-6 absolute left-4" /> */}
+                    <input type="text" placeholder="Search by token or asset ID" className="w-full bg-brandDark/10 px-4 py-2 rounded-xl focus:outline-none text-xl" onChange={handleSearchChange} />
+                  </div>
+
+                  <Transition.Child
                     as={Fragment}
-                    enter="ease-out duration-200"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
+                    enter="ease-out duration-500"
+                    enterFrom="translate-y-10 blur"
+                    enterTo="translate-y-0 blur-none"
                     leave="ease-in duration-200"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                >
-                    <Dialog.Panel className="w-full max-w-md h-full transform overflow-hidden rounded-2xl p-2 text-left align-middle transition-all">
-
-                    <Dialog.Title as="h3" className="text-[2.5rem] sm:text-5xl pt-4 pb-4 md:pb-8 font-bold text-black dark:text-brandLight">Select a token</Dialog.Title>
-
-                    {/* Main Content Container */}
-                    <div className="flex flex-col max-h-full min-h-full">
-
-                      {/* Search */}
-                      <div className="relative w-full flex flex-col items-center">
-                        {/* <SearchIcon className="h-6 absolute left-4" /> */}
-                        <input type="text" placeholder="Search by token or asset ID" className="w-full bg-brandDark/10 px-4 py-2 rounded-xl focus:outline-none text-xl" onChange={handleSearchChange} />
-                      </div>
-
-                      <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-500"
-                        enterFrom="translate-y-10 blur"
-                        enterTo="translate-y-0 blur-none"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100 translate-y-0 blur"
-                        leaveTo="opacity-0 translate-y-10 blur"
-                      >
-                        <div className="relative h-[calc(100svh-164px)]">
-                          {filteredTokens && (
-                            <div className="absolute w-full h-full min-h-full max-h-full left-0 top-0">
-                              <AutoSizer>
-                                {({ height, width }) => (
-                                  <List
-                                    className="List"
-                                    height={height}
-                                    itemCount={filteredTokens.length}
-                                    itemSize={68}
-                                    width={width}
-                                  >
-                                    {tokenRow}
-                                  </List>
-                                )}
-                              </AutoSizer>
-                            </div>
-                          )}
+                    leaveFrom="opacity-100 translate-y-0 blur"
+                    leaveTo="opacity-0 translate-y-10 blur"
+                  >
+                    <div className="relative h-[calc(100svh-164px)]">
+                      {filteredTokens && (
+                        <div className="absolute w-full h-full min-h-full max-h-full left-0 top-0">
+                          <AutoSizer>
+                            {({ height, width }) => (
+                              <List
+                                className="List"
+                                height={height}
+                                itemCount={filteredTokens.length}
+                                itemSize={68}
+                                width={width}
+                              >
+                                {tokenRow}
+                              </List>
+                            )}
+                          </AutoSizer>
                         </div>
-
-                      </Transition.Child>
-
+                      )}
                     </div>
-                    </Dialog.Panel>
-                </Transition.Child>
+
+                  </Transition.Child>
+
                 </div>
+                </Dialog.Panel>
+            </Transition.Child>
             </div>
-            </Dialog>
-        </Transition>
-     );
+        </div>
+        </Dialog>
+    </Transition>
+  );
 }
 
 export default TokenSelectorModal;
