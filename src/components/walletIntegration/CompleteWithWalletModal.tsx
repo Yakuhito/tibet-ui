628183ce51001af088a637505e52f87516ec8565
Jonathan <jonathanlangton3@gmail.com>
2023-09-27 12:03:19 +0100

    Added close button in the top right & made consistent with new modal style (on smaller devices)

--- src/components/walletIntegration/CompleteWithWalletModal.tsx
+++ src/components/walletIntegration/CompleteWithWalletModal.tsx
@@ -112,7 +112,7 @@ const CompleteWithWalletModal = () => {
           enter="ease-out duration-300"
           enterFrom="opacity-0"
           enterTo="opacity-100"
-          leave="ease-in duration-200"
+          leave="ease-in duration-2 00"
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
       >
