f873a7374807d79678673849fbcb5c351c855e5f
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2023-10-03 17:17:23 +0100

    Improved styling

--- src/components/analytics_components/Table/index.tsx
+++ src/components/analytics_components/Table/index.tsx
@@ -22,29 +22,22 @@ function Table({ data, columns }: {
   const table = useReactTable({
     data,
     columns,
-    // Pipeline
     getCoreRowModel: getCoreRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
-    //
   })
 
   return (
-    <div className="w-full max-w-full overflow-x-auto flex flex-col">
-      <ul className="flex gap-2 pb-4 sm:px-4 text-xl sticky left-0">
-        <li className="cursor-pointer hover:opacity-80 font-medium bg-black text-brandLight rounded-full px-8 py-2 border border-transparent">Swaps</li>
-        <li className="cursor-pointer hover:opacity-80 font-medium text-black/80 rounded-full px-8 py-2 border">Add</li>
-        <li className="cursor-pointer hover:opacity-80 font-medium text-black/80 rounded-full px-8 py-2 border">Remove</li>
-      </ul>
+    <div className="relative w-full max-w-full overflow-x-auto flex flex-col">
       <table className="w-full">
         <thead className="sticky top-0 left-0">
           {table.getHeaderGroups().map(headerGroup => (
             <tr key={headerGroup.id}>
               {headerGroup.headers.map(header => {
                 return (
-                  <th key={header.id} colSpan={header.colSpan} className="text-left text-xl text-brandDark font-medium p-4 px-0 sm:px-4 whitespace-nowrap first:rounded-l-full last:rounded-r-full">
+                  <th key={header.id} colSpan={header.colSpan} className="text-left group font-medium border-b text-sm border-brandDark/10 py-4 whitespace-nowrap first:rounded-l-full last:rounded-r-full">
                     {header.isPlaceholder ? null : (
-                      <div className="bg-brandDark/10 px-8 py-2 text-center rounded-full mr-4 last:mr-0">
+                      <div className="py-1 inline rounded-full mr-4 group-last:mr-0 opacity-70">
                         {flexRender(
                           header.column.columnDef.header,
                           header.getContext()
@@ -60,11 +53,11 @@ function Table({ data, columns }: {
         <tbody>
           {table.getRowModel().rows.map(row => {
             return (
-              <tr key={row.id} className="hover:bg-brandDark/10 transition border-b border-neutral-100">
+              <tr key={row.id} className="hover:bg-brandDark/5 hover:border-b-transparent transition border-b border-brandDark/10">
                   {row.getVisibleCells().map(cell => {
                     return (
-                      <td key={cell.id} className="min-w-[120px] whitespace-nowrap first:rounded-l-full last:rounded-r-full font-medium">
-                        <Link className="px-4 sm:px-8 py-6 block" href={process.env.NEXT_PUBLIC_SPACESCAN_BASE_URL + data[cell.row.index].coin_id} target='_blank'>
+                      <td key={cell.id} className="min-w-[120px] whitespace-nowrap font-medium text-sm">
+                        <Link className="py-4 px-1 block" href={process.env.NEXT_PUBLIC_SPACESCAN_BASE_URL + data[cell.row.index].coin_id} target='_blank'>
                           {flexRender(
                             cell.column.columnDef.cell,
                             cell.getContext()
@@ -79,54 +72,42 @@ function Table({ data, columns }: {
         </tbody>
       </table>
       <div className="h-2" />
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 font-medium text-sm">
         <button
-          className="border rounded p-1"
+          className=""
           onClick={() => table.setPageIndex(0)}
           disabled={!table.getCanPreviousPage()}
         >
           {'<<'}
         </button>
         <button
-          className="border rounded p-1"
+          className="p-1"
           onClick={() => table.previousPage()}
           disabled={!table.getCanPreviousPage()}
         >
           {'<'}
         </button>
         <button
-          className="border rounded p-1"
+          className="p-1"
           onClick={() => table.nextPage()}
           disabled={!table.getCanNextPage()}
         >
           {'>'}
         </button>
         <button
-          className="border rounded p-1"
+          className="p-1"
           onClick={() => table.setPageIndex(table.getPageCount() - 1)}
           disabled={!table.getCanNextPage()}
         >
           {'>>'}
         </button>
-        <span className="flex items-center gap-1">
+        
+        <span className="flex items-center gap-1 mx-4">
           <div>Page</div>
-          <strong>
             {table.getState().pagination.pageIndex + 1} of{' '}
             {table.getPageCount()}
-          </strong>
         </span>
-        <span className="flex items-center gap-1">
-          | Go to page:
-          <input
-            type="number"
-            defaultValue={table.getState().pagination.pageIndex + 1}
-            onChange={e => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0
-              table.setPageIndex(page)
-            }}
-            className="border p-1 rounded w-16"
-          />
-        </span>
+        
         <select
           value={table.getState().pagination.pageSize}
           onChange={e => {
@@ -140,8 +121,6 @@ function Table({ data, columns }: {
           ))}
         </select>
       </div>
-      <div>{table.getRowModel().rows.length} Rows</div>
-      <pre>{JSON.stringify(table.getState().pagination, null, 2)}</pre>
     </div>
   )
 }
