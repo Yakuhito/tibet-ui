b1664d521511f6a2da4510474240f933a58ff223
Jonathan <41968179+JonathanLangton1@users.noreply.github.com>
2024-02-20 11:26:29 +0000

    Disable vercel web analytics when developing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -88,7 +88,8 @@ export default function App({ Component, pageProps }: AppProps) {
           <div className="flex flex-col px-4">
             <div className="pt-12 pb-[96px]">
               <Component {...pageProps}  />
-              <Analytics />
+              {/* Disable vercel web analytics when developing */}
+              {!!process?.env?.VERCEL_ENV && <Analytics />}
             </div>
             <footer className="absolute bottom-0 w-full left-0 pb-6 pt-12 text-center text-brandDark mt-full mx-auto flex flex-col items-center">
               <a href="https://twitter.com/TibetSwap" target="_blank" rel="noopener noreferrer" className="underline ml-1">
