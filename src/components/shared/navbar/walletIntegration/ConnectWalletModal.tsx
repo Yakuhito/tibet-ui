440485ead4ddca6e1278565c000d91a3b506a01e
Yak <y@kuhi.to>
2026-06-23 11:53:29 +0300

    remove legacy wallet from WalletConnect hint/description

--- src/components/shared/navbar/walletIntegration/ConnectWalletModal.tsx
+++ src/components/shared/navbar/walletIntegration/ConnectWalletModal.tsx
@@ -93,7 +93,8 @@ function ConnectWalletModal({ isOpen, setIsOpen, isWalletOnWrongChain }: Connect
                             <WalletConnectIcon className="w-10 h-10 dark:fill-brandLight" />
                             <div>
                                 <p className="font-medium text-lg">Wallet Connect</p>
-                                <p className="font-normal text-xs">Sage | Ozone | Reference Wallet</p>
+                                {/* <p className="font-normal text-xs">Sage | Ozone | Legacy Wallet</p> */}
+                                <p className="font-normal text-xs">Sage | Ozone </p>
                             </div>
                         </div>
                         <button className={`
